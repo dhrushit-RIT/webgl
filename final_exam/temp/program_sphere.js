af88be7fd29d48f821940f035d198e6e5bdacdce
Dhrushit Suresh Raval <dr9703@rit.edu>
2021-05-06 19:14:12 -0400

    updating the shaders

--- final_exam/temp/program_sphere.js
+++ final_exam/temp/program_sphere.js
@@ -1,17 +1,24 @@
 var sphere_vertex_shader = `
     #version 300 es
+
+    // Vertex Attributes
     in vec3 aVertexPosition;
+
+    // Transforms
+    uniform mat4 modelT;
+    uniform mat4 viewT;
+    uniform mat4 projT;
 
     void main()
     {
-        gl_Position = aVertexPosition;
+        gl_Position = projT * viewT * modelT * vec4 (aVertexPosition, 1.0);
     }
 `;
 
 var sphere_fragment_shader = `
     #version 300 es
     precision mediump float;
-    
+
     // Color that is the result of this shader
     out vec4 fragColor;
 
--- final_exam/temp/sphere.vs.glsl
+++ final_exam/temp/sphere.vs.glsl
@@ -1,6 +1,13 @@
-#version 300 es
+ #version 300 es
+
+// Vertex Attributes
 in vec3 aVertexPosition;
 
+// Transforms
+uniform mat4 modelT;
+uniform mat4 viewT;
+uniform mat4 projT;
+
 void main() {
-    gl_Position = aVertexPosition;
+    gl_Position = projT * viewT * modelT * vec4(aVertexPosition, 1.0);
 }
