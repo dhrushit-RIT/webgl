2716a484a87e56b964bb890ac9710f26014385d6
Dhrushit Suresh Raval <dr9703@rit.edu>
2021-05-06 09:08:46 -0400

    initial

---
+++ final_exam/program_table.js
@@ -0,0 +1,21 @@
+var table_vertex_shader = `
+    #version 300 es
+    in vec3 aVertexPosition;
+
+    void main()
+    {
+        gl_Position = aVertexPosition;
+    }
+`;
+
+var table_fragment_shader = `
+    #version 300 es
+    precision mediump float;
+    
+    // Color that is the result of this shader
+    out vec4 fragColor;
+
+    void main(void) {
+        fragColor = vec4 (0.5, 0.5, 0.5, 1.0 );
+    }
+`;
---
+++ final_exam/sphere.vs.glsl
@@ -0,0 +1,6 @@
+#version 300 es
+in vec3 aVertexPosition;
+
+void main() {
+    gl_Position = aVertexPosition;
+}
